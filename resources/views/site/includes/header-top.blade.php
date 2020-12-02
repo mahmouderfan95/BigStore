9012a760d41074809f2b8faffac5b619801f1208
mahmoud erfan <mahmoudesmailgamal0@gmail.com>
2020-12-02 02:59:33 +0200

    S

---
+++ resources/views/site/includes/header-top.blade.php
@@ -0,0 +1,23 @@
+<div class="header-top">
+    <div class="container">
+        <div class="ht-left">
+            <a href="#" class="row user-service pr-5 pl-2">
+                <h5>خدمة العملاء </h5>
+                <i class="fa fa-user mr-2" style="font-size: 20px;"></i>
+            </a>
+            <a href="#" class="row user-service pr-5 pl-2">
+                <h5>بدأ البيع</h5>
+                <i class="fa fa-cart-arrow-down mr-2" style="font-size: 20px;"></i>
+            </a>
+            <a  href="#" class="row user-service pr-5 pl-2">
+                <h5>تتبع مشتريتك</h5>
+                <i class="fa fa-shopping-cart mr-2" style="font-size: 20px;"></i>
+            </a>
+        </div>
+        <div class="ht-right">
+            <a href="#" class="login-panel">تسجيل الدخول<i class="fa fa-user mr-2"></i></a>
+            <div class="lan-selector text-white"> العربية
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/site/includes/nav-item.blade.php
@@ -0,0 +1,37 @@
+<div class="nav-item ">
+    <div class="container">
+        <div class="nav-depart">
+            <div class="depart-btn">
+                <span class="ml-3">عرض كل المنتجات</span>
+                <i class="fa fa-bars"></i>
+                <ul class="depart-hover">
+                    <li class="active"><a href="#">Women’s Clothing</a></li>
+                    <li><a href="#">Men’s Clothing</a></li>
+                    <li><a href="#">Underwear</a></li>
+                    <li><a href="#">Kids Clothing</a></li>
+                    <li><a href="#">Brand Fashion</a></li>
+                    <li><a href="#">Accessories/Shoes</a></li>
+                    <li><a href="#">Luxury Brands</a></li>
+                    <li><a href="#">Brand Outdoor Apparel</a></li>
+                </ul>
+            </div>
+        </div>
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="./index.html">العاب</a></li>
+                <li><a href="./shop.html">الكترونية</a></li>
+                <li><a href="#">رياضة</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Mens</a></li>
+                        <li><a href="#">Womens</a></li>
+                        <li><a href="#">Kids</a></li>
+                    </ul>
+                </li>
+                <li><a href="./blog.html">منزل</a></li>
+                <li><a href="./contact.html">ملابس و أزياء</a></li>
+                <li class="active"><a href="#">الرئيسية</a></li>
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+    </div>
+</div>
